f7fca123450fb1e25a45042ce999faddd232e86d
NICOLLEMON27 <142424875+NICOLLEMON27@users.noreply.github.com>
2024-02-13 12:51:37 -0500

    Add files via upload

---
+++ LibServices/clsUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace pkgServices
+{
+    public static class clsUtilities {
+        public static T opAdd<T>(T prmOp1, T prmOp2) where T : struct, IConvertible
+        {
+            if (typeof(T) == typeof(double))
+            {
+                return (T)Convert.ChangeType(((double)Convert.ChangeType(prmOp1, typeof(double)) +
+                    (double)Convert.ChangeType(prmOp2, typeof(double))), typeof(T));
+            }
+            if (typeof(T) == typeof(char))
+            {
+                return (T)Convert.ChangeType(((char)Convert.ChangeType(prmOp1, typeof(char)) +
+                    (char)Convert.ChangeType(prmOp2, typeof(char))), typeof(T));
+            }
+                if (typeof(T) == typeof(int)) { 
+                    return (T)Convert.ChangeType(((int)Convert.ChangeType(prmOp1, typeof(int)) +
+                        (int)Convert.ChangeType(prmOp2, typeof(int))), typeof(T));
+            }
+
+
+            return default;
+        }
+    }
+}
---
+++ uTestDemo/uTestUtilities.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using pkgServices;
+
+namespace uTestDemo
+{
+    [TestClass]
+    public class uTestUtilities
+    {
+        [TestMethod]
+        public void testAddTwoChar()
+        {
+            #region Setup
+            char varExpectedResult =(char) 10;
+            char varActualResult = default;
+            #endregion
+            #region Test
+            varActualResult = clsUtilities.opAdd((char)4, (char)6);
+            #endregion
+            #region Assert
+            Assert.AreEqual(varExpectedResult, varActualResult);
+            #endregion
+        }
+    }
+}
