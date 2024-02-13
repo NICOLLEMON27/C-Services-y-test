e3e0fdf0bec91d03c7cc1a68b898f63614551e35
NICOLLEMON27 <142424875+NICOLLEMON27@users.noreply.github.com>
2024-02-13 12:52:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using pkgServices;
+namespace appConsDemo
+{
+    internal class Program
+    {
+        static T opReadNumber<T>(string prmLabel)
+        {
+            Console.Write(prmLabel + ":");
+
+            return (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+        }
+     
+        static void Main()
+        {
+            Console.WriteLine(clsUtilities.opAdd(opReadNumber<int>("a First Number"), opReadNumber<int>("a Second Number")));
+            Console.ReadKey();
+        }
+    }
+}
